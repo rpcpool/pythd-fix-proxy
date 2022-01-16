4c30df8d8dfad512ad1530fffb05ee4f766a70f9
batphonghan <vanhungdeveloper@gmail.com>
2022-01-16 12:12:05 +0700

    Remove set 246 tag

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -282,8 +282,9 @@ func (app *Application) makeFix42MarketDataRequest(symbol string) *quickfix.Mess
 	relatedSym := fix42mdr.NewNoRelatedSymRepeatingGroup()
 	relatedSym.Add().SetSymbol(symbol)
 	request.SetNoRelatedSym(relatedSym)
-	request.SetString(quickfix.Tag(146), "1")
-	request.Header.SetString(quickfix.Tag(5000), "0")
+
+	// request.SetString(quickfix.Tag(146), "1")
+	request.SetString(quickfix.Tag(5000), "0")
 
 	// request.SetMDUpdateType(enum.MDUpdateType_FULL_REFRESH)
 
