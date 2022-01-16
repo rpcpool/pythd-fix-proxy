68f783475a56bfcbfc05be02b10cae34f779b5ec
batphonghan <vanhungdeveloper@gmail.com>
2022-01-16 10:31:24 +0700

    Remove SetMDUpdateType

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -285,7 +285,7 @@ func (app *Application) makeFix42MarketDataRequest(symbol string) *quickfix.Mess
 	request.SetString(quickfix.Tag(146), "1")
 	request.Header.SetString(quickfix.Tag(5000), "0")
 
-	request.SetMDUpdateType(enum.MDUpdateType_FULL_REFRESH)
+	// request.SetMDUpdateType(enum.MDUpdateType_FULL_REFRESH)
 
 	return request.ToMessage()
 }
